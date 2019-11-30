c26a7c7c2f6045ddfd3b53db9fce2c29572f03bd
Anna <annak@iprosis.com>
2019-11-30 21:16:27 +0200

    ff

--- webcomponent.js
+++ webcomponent.js
@@ -228,7 +228,7 @@
 		//	if ("sdata" in changedProperties) {
 				this.data = changedProperties["sdata"];
 				console.log(`${this._props["sdata"]}`);
-				listik = setData();
+				listik = this.setData();
 				autocomplete(shadow.querySelector('#myInput'), listik);
 		//	}
 		}
