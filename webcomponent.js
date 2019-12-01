2abe8f5704618dc72310b1e041628b71c0abd153
Anna <annak@iprosis.com>
2019-12-01 13:46:47 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -1,40 +1,35 @@
 (function() {
+	console.log(1);
 	let tmpl = document.createElement('template');
+	console.log(2);
 	tmpl.innerHTML = `
-
 		<style>
 			* {
 			box-sizing: border-box;
 			}
-
 			body {
 			font: 16px Arial;  
 			}
-
 			/*the container must be positioned relative:*/
 			.autocomplete {
 			position: relative;
 			display: inline-block;
 			}
-
 			input {
 			border: 1px solid transparent;
 			background-color: #f1f1f1;
 			padding: 10px;
 			font-size: 16px;
 			}
-
 			input[type=text] {
 			background-color: #f1f1f1;
 			width: 100%;
 			}
-
 			input[type=submit] {
 			background-color: DodgerBlue;
 			color: #fff;
 			cursor: pointer;
 			}
-
 			.autocomplete-items {
 			position: absolute;
 			border: 1px solid #d4d4d4;
@@ -46,109 +41,85 @@
 			left: 0;
 			right: 0;
 			}
-
 			.autocomplete-items div {
 			padding: 10px;
 			cursor: pointer;
 			background-color: #fff; 
 			border-bottom: 1px solid #d4d4d4; 
 			}
-
 			/*when hovering an item:*/
 			.autocomplete-items div:hover {
 			background-color: #e9e9e9; 
 			}
-
 			/*when navigating through the items using the arrow keys:*/
 			.autocomplete-active {
 			background-color: DodgerBlue !important; 
 			color: #ffffff; 
-
 		</style>
-
-		<div class="autocomplete" id="container" style="width:300px;">
+		<div class="autocomplete" id="container">
 			<input id="myInput" type="text" name="myCountry" placeholder="Country">
 		</div>
-
-
 	`;
-
 	class ISearch extends HTMLElement {
 		constructor() {
 			super();
-			
 			let shadow = this.attachShadow({ mode: 'open' });
 			shadow.appendChild(tmpl.content.cloneNode(true));
-			var data = [];
-			
-			
-			
-			function autocomplete(inp, arr) {
-				var currentFocus;
-
-
+			let that = this;
+			function autocomplete(inp, that) {
+				let currentFocus;
 				inp.addEventListener('input', function(e) {
-					var a,
-						b,
-						i,
-						val = this.value;
-
-					//closeAllLists();
-					if (!val) {
-						return false;
-					}
+					let val = this.value;
+					closeAllLists();
 					currentFocus = -1;
-					a = document.createElement('DIV');
-					a.setAttribute('id', this.id + 'autocomplete-list');
-					a.setAttribute('class', 'autocomplete-items');
+					let a = document.createElement('div');
+					a.id = this.id + 'autocomplete-list';
+					a.classList.add('autocomplete-items');
 					this.parentNode.appendChild(a);
-					for (i = 0; i < arr.length; i++) {
+					for (let i = 0; i < that.dataModel.length; i++) {
 						if (
-							arr[i].substr(0, val.length).toUpperCase() ==
-							val.toUpperCase()
+							that.dataModel[i]
+								.substr(0, val.length)
+								.toUpperCase() == val.toUpperCase()
 						) {
-							b = document.createElement('DIV');
+							let b = document.createElement('div');
 							b.innerHTML =
 								'<strong>' +
-								arr[i].substr(0, val.length) +
-								'</strong>';
-							b.innerHTML += arr[i].substr(val.length);
-							b.innerHTML +=
-								"<input type='hidden' value='" + arr[i] + "'>";
-
+								that.dataModel[i].substr(0, val.length) +
+								'</strong>' +
+								that.dataModel[i].substr(val.length);
 							b.addEventListener('click', function(e) {
-								inp.value = this.getElementsByTagName(
-									'input'
-								)[0].value;
+								that.selectedValue = this.innerText;
+								inp.value = this.innerText;
 								closeAllLists();
 							});
 							a.appendChild(b);
 						}
 					}
 				});
-
-				// inp.addEventListener('keydown', function(e) {
-				// 	var x = shadow.getElementById(
-				// 		this.id + 'autocomplete-list'
-				// 	);
-				// 	if (x) x = x.getElementsByTagName('div');
-				// 	if (e.keyCode == 40) {
-				// 		currentFocus++;
-				// 		addActive(x);
-				// 	} else if (e.keyCode == 38) {
-				// 		currentFocus--;
-
-				// 		addActive(x);
-				// 	} else if (e.keyCode == 13) {
-				// 		e.preventDefault();
-				// 		if (currentFocus > -1) {
-				// 			if (x) x[currentFocus].click();
-				// 		}
-				// 	}
-				// });
+				inp.addEventListener('keydown', function(e) {
+					var x = shadow.getElementById(
+						this.id + 'autocomplete-list'
+					);
+					if (x) x = x.getElementsByTagName('div');
+					// arrow down
+					if (e.keyCode == 40) {
+						currentFocus++;
+						addActive(x);
+						// arrow up
+					} else if (e.keyCode == 38) {
+						currentFocus--;
+						addActive(x);
+					} else if (e.keyCode == 13) {
+						//enter
+						e.preventDefault();
+						if (currentFocus > -1) {
+							if (x) x[currentFocus].click();
+						}
+					}
+				});
 				function addActive(x) {
 					if (!x) return false;
-
 					removeActive(x);
 					if (currentFocus >= x.length) currentFocus = 0;
 					if (currentFocus < 0) currentFocus = x.length - 1;
@@ -160,81 +131,103 @@
 					}
 				}
 				function closeAllLists(elmnt) {
-					console.log("enter func");
-					var x = shadow.getElementsByClassName('autocomplete-items');
+					var xContainer = shadow.getElementById('container');
+					var x = xContainer.getElementsByClassName(
+						'autocomplete-items'
+					);
 					for (var i = 0; i < x.length; i++) {
-						console.log("enter func FOR");
 						if (elmnt != x[i] && elmnt != inp) {
-							console.log("enter func for IF");
 							x[i].parentNode.removeChild(x[i]);
 						}
 					}
 				}
-
-			
-
 				document.addEventListener('click', function(e) {
-					
-					//closeAllLists(e.target);
+					closeAllLists(e.target);
 				});
 			}
-
-			function getData(result){
-				// countries = [];
-				//mm = ISearch.sdata;
-				result.forEach(function(element) {
-		
-					data.push(element);	
-					//data=ISearch.sdata;
-				 }
-				 
-			);
-			}
-
-			function setData(){
-				return data;
-			};
-
-			var countries = [
-				'Afghanistan',
-				'Albania',
-				'Algeria',
-				'Andorra',
-				'Angola',
-				'Anguilla',
-				'Antigua'
-			];
-
-			//var countries = [];
-			getData(["ak","lk","yk"]);//ISearch.sdata);
-			var listik = setData();
-			autocomplete(shadow.querySelector('#myInput'), listik);
-
-			if (this._alive) {
-				return;
-			} else {
-				this._alive = true;
-			}
-			this._props = {};
-		} // end constructor
-
-
-		onCustomWidgetBeforeUpdate(changedProperties) {
-			this._props = { ...this._props, ...changedProperties };
+			autocomplete(shadow.querySelector('#myInput'), that);
 		}
-
-		onCustomWidgetAfterUpdate(changedProperties) {
-			if ("sdata" in changedProperties) {
-				this.data = changedProperties["sdata"];
-				console.log(`${this._props["sdata"]}`);
-
-				var mm =this.shadowRoot.querySelector('#container');
-				var mm1 =mm.childNodes[1];
-				this.autocomplete(mm1, this.data);
-			}
+		selectedValue = '';
+		dataModel = [
+			'Afghanistan',
+			'Albania',
+			'Algeria',
+			'Andorra',
+			'Angola',
+			'Anguilla',
+			'Antigua'
+		];
+		countries = [
+			'Abkhazia',
+			'Afghanistan',
+			'Akrotiri and Dhekelia',
+			'Albania',
+			'Algeria',
+			'American Samoa',
+			'Andorra',
+			'Angola',
+			'Anguilla',
+			'Antigua and Barbuda',
+			'Argentina',
+			'Armenia',
+			'Brazil',
+			'British Virgin Islands',
+			'Brunei',
+			'Bulgaria',
+			'Cambodia',
+			'Costa Rica',
+			'Croatia',
+			'Cuba',
+			"Côte d'Ivoire",
+			'Dominican Republic',
+			'Federated States of Micronesia',
+			'Fiji',
+			'Finland',
+			'Greece',
+			'Guinea',
+			'Haiti',
+			'Israel',
+			'Hungary',
+			'Iceland',
+			'India'
+		];
+		capitals = [
+			'Sukhumi',
+			'Jeusalem',
+			'Kabul',
+			'Episkopi Cantonment',
+			'Tirana',
+			'Algiers',
+			'Pago Pago',
+			'Moskow',
+			'Andorra la Vella',
+			'Luanda',
+			'The Valley',
+			"St. John's",
+			'Buenos Aires',
+			'Yerevan',
+			'Oranjestad',
+			'Georgetown',
+			'Canberra',
+			'Asmara',
+			'Tallinn',
+			'Bissau',
+			'Georgetown',
+			'Port-au-Prince',
+			'Tegucigalpa',
+			'Budapest',
+			'Reykjavík',
+			'New Delhi'
+		];
+		getValue() {
+			return this.selectedValue;
 		}
-
-
+		getDataModel() {
+			return this.dataModel;
+		}
+		setDataModel(newdata) {
+			this.dataModel = newdata;
+		}
 	}
 	/* Define web component - input: tag and class */
 	customElements.define('com-iprosis-sample-search', ISearch);
