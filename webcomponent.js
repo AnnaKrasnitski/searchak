c87042cb422a0918384f5f88b0692cd064c5d69c
Anna <annak@iprosis.com>
2020-01-02 11:02:26 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -204,17 +204,17 @@
 
 		/* setter of datalist to autocomplete*/
 		setMembers(newdata,disp) {
-			that.display = disp;
+			this.display = disp;
 				for (let index = 0; index < newdata.length; index++) {
 					
 				//	if (disp == 'k'){
-					that.dataModel[0][index] = newdata[index].displayId;
+					this.dataModel[0][index] = newdata[index].displayId;
 						
 				//	} else if (disp == 't'){
-					that.dataModel[1][index] = newdata[index].description;
+					this.dataModel[1][index] = newdata[index].description;
 						
 				//	} else {
-					that.dataModel[2][index] = newdata[index].displayId + ' ' + newdata[index].description
+					this.dataModel[2][index] = newdata[index].displayId + ' ' + newdata[index].description
 				//	}
 							
 				};	
